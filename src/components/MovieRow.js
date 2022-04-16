d520f8b25dc307bf2ae4d71dcd19b88d549120ef
Jeecambui <jeferson_cambui@hotmail.com>
2022-04-16 00:01:01 -0300

    Atualizando o projeto

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,29 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import './MovieRow.css';
 import { NavigateBefore } from "@mui/icons-material";
 import { NavigateNext } from "@mui/icons-material";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({title, items}) => {
+    const [scrollX, setScrollX]= useState(-400);
+
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0){
+            x = 0;
+        }
+        setScrollX(x);
+    }
+
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listW = items.results.length * 150;
+        if((window.innerWidth - listW) > x) {
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScrollX(x);
+    }
+
     return (
         <div className="movieRow">
             <h2>{title}</h2>
-            <div className="movieRow--left">
+            <div className="movieRow--left" onClick={handleLeftArrow}>
                 <NavigateBefore style={{fontSize: 50}} />
             </div>
-            <div className="movieRow--right">
+            <div className="movieRow--right" onClick={handleRightArrow}>
                 <NavigateNext style={{fontSize: 50}} />
             </div>
 
             <div className="movieRow--listarea">
-                <div className="movieRow-list">
-                    {items.results.length > 0 && items.results.map((item, key)=>(
+                <div className="movieRow--list" style={{
+                    marginLeft: scrollX,
+                    width: items.results.length * 150
+                }}>
+                    {items.results.length > 0 && items.results.map((item,key)=>(
                         <div className="movieRow--item">
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
                         </div>
-
-                    ))}
+                    ))};
                 </div>
+
             </div>
         </div>
     );
